1384fe6f7ddcfba06563dd1e4faa1bf17d87c133
Arthur <dypis123@gmail.com>
2024-06-15 17:50:35 +0400

    KS-1-4 Yuri Gagarin in space more info

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main(){
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("– $length – расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах;")
+    println("– $age – возраст Гагарина на момент полета (целых лет);")
+    println("– $partOfDay – какую часть дня Гагарин провел в космосе;")
+    println("– $seconds – количество секунд которое Гагарин провел в космосе;")
+    println("– $partOfYear – какую часть года Гагарин провел в космосе;")
+    println("– $apogee – апогей орбиты Гагарина в метрах.")
+}
